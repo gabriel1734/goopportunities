b6b8750aae30718714a1b45e2659cc2379118be1
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-05-31 22:58:08 -0300

    :sparkles: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gabriel1734/goopportunities/router"
+import (
+	"github.com/gabriel1734/goopportunities/config"
+	"github.com/gabriel1734/goopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
