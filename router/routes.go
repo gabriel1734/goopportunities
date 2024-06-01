93fba0614994e3d48a5bd31d19c951e607325d11
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-05-31 22:14:23 -0300

    :sparkles: opening handler added

--- router/routes.go
+++ router/routes.go
@@ -1,21 +1,21 @@
 package router
 
 import (
-	"github.com/gabriel1734/goopportunities/handler"
+	"github.com/gabriel1734/goopportunities/handler/opening"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening", opening.ShowOpeningHandler)
 
-		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.POST("/opening", opening.CreateOpeningHandler)
 
-		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.DELETE("/opening", opening.DeleteOpeningHandler)
 
-		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.PUT("/opening", opening.UpdateOpeningHandler)
 
-		v1.GET("/openings", handler.ListOpeningHandler)
+		v1.GET("/openings", opening.ListOpeningHandler)
 	}
 }
